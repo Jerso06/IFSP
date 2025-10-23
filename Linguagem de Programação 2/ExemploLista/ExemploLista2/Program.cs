7b6b208990c2c55c02c425fbb5ad8620d855c168
Jerso06 <jedsonhenrique2006@gmail.com>
2025-10-22 21:09:17 -0300

    Aula c#, listas (22/10)

---
+++ Linguagem de Programação 2/ExemploLista/ExemploLista2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploLista2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //cria uma lista com marcas/fabricantes de carros
+            List<string> carros = new List<string>();
+            //insere elementos (marcas) no final da lista;
+            carros.Add("Volvo");
+            carros.Add("BMW");
+            carros.Add("Ford");
+            carros.Add("Honda");
+            //imprime todos os elemento da lista
+            foreach (string carro in carros)
+            {
+                Console.WriteLine(carro);
+            }
+
+        }
+    }
+}
