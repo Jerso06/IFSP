47151999ff12892454944b537a0e3acaaa71d017
Jerso06 <jedsonhenrique2006@gmail.com>
2026-03-06 21:31:05 -0300

    Aula estrutura de dados (lista encadeada)

---
+++ Estrutura de Dados 1/exemplo1ListaEncadeada/main.c
@@ -0,0 +1,82 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct _node {
+
+    int val;
+    struct _node *next;
+} Node;
+
+
+typedef struct _linked_list {
+
+    Node *begin;
+
+} LinkedList;
+
+
+
+Node *Node_create(int val) {
+    Node *node = (Node*) calloc(5, sizeof(Node));
+    node->val = val;
+    node->next = NULL;
+    return node;
+}
+
+
+
+LinkedList *LinkedList_create() {
+
+    LinkedList *L = (LinkedList *) calloc(1, sizeof(LinkedList));
+    L->begin = NULL;
+    return L;
+}
+
+void LinkedList_add_first(LinkedList *L, int val) {
+
+    Node *p = Node_create(val);
+
+    if (L->begin == NULL){   //se a lista est� vazia
+
+        p->next = L->begin;
+        L->begin = p;
+
+    } else {
+        p->next = L->begin;
+        L->begin = p;
+    }
+
+}
+
+
+
+void LinkedList_print(LinkedList *L) {
+    Node *p = L->begin;
+    printf("L -> ");
+    // enquanto p n�o chegou ao fim da lista, isto �,
+    // enquanto p estiver apontando para um n� da lista
+
+    while (p != NULL) {
+        printf("%d ->", p->val);
+        p = p->next;
+    }
+    printf(" NULL  ");
+}
+
+
+
+int main (){
+
+LinkedList *La = LinkedList_create();
+
+LinkedList_add_first(La,5);
+LinkedList_add_first(La,4);
+LinkedList_add_first(La,2);
+LinkedList_add_first(La,10);
+LinkedList_add_first(La,20);
+LinkedList_print(La);
+
+
+return (0);
+}
