1c8a1cb53152ad8d70676ce75c999028801365d0
Jerso06 <jedsonhenrique2006@gmail.com>
2026-04-02 00:57:36 -0300

    ajustes exercício da calculadora

--- Desenvolvimento Web 1/abril/01/Calculadora/script.js
+++ Desenvolvimento Web 1/abril/01/Calculadora/script.js
@@ -1,77 +1,130 @@
-function clean(){
+function clean() {
     document.getElementById("display").innerHTML = "";
 }
 
-function inserir(num){
+function inserir(num) {
     document.getElementById("display").innerHTML += num;
 }
 
-function back(){
+function back() {
     let display = document.getElementById("display").innerHTML;
 
-    document.getElementById("display").innerHTML = display.substring(0,display.length-1);
+    document.getElementById("display").innerHTML = display.substring(0, display.length - 1);
 }
 
-function calcular(){
+function calcular() {
     let resultado = document.getElementById("display").innerHTML;
     resultado = resultado.replaceAll(',', '.');
-    if(resultado.indexOf('+') != -1){
-        let n1 = Number(resultado.substring(0,resultado.indexOf('+')));
-        let n2 = Number(resultado.substring(resultado.indexOf('+')+1, resultado.length));
-        somar(n1, n2);
-    }else if(resultado.indexOf('-') != -1){
-        let n1;
-        if(resultado.indexOf('-') == 0){
-            n1 = Number(resultado.substring(0,resultado.indexOf('-')))
+    let n1, n2;
+
+    if (resultado.length != 0 && verificaSinais(resultado)) {
+        if (resultado.indexOf('+') != -1) {
+            n1 = Number(resultado.substring(0, resultado.indexOf('+')));
+            n2 = Number(resultado.substring(resultado.indexOf('+') + 1, resultado.length));
+            somar(n1, n2);
+        } else if (resultado.indexOf('-') != -1) {
+            if (resultado.indexOf('-') == 0) {
+                n1 = Number(resultado.substring(0, resultado.indexOf('-')))
+            } else {
+                n1 = Number(resultado.substring(0, resultado.indexOf('-')));
+            }
+            n2 = Number(resultado.substring(resultado.indexOf('-') + 1, resultado.length));
+            subtrair(n1, n2)
+        } else if (resultado.indexOf('/') != -1) {
+            n1 = Number(resultado.substring(0, resultado.indexOf('/')));
+            n2 = Number(resultado.substring(resultado.indexOf('/') + 1, resultado.length));
+            dividir(n1, n2)
+        } else if (resultado.indexOf('*') != -1) {
+            n1 = Number(resultado.substring(0, resultado.indexOf('*')));
+            n2 = Number(resultado.substring(resultado.indexOf('*') + 1, resultado.length));
+            multiplicar(n1, n2)
+        } else {
+            resultado = resultado.replaceAll('.', ',');
+            document.getElementById("display").innerHTML = resultado;
         }
-        n1 = Number(resultado.substring(0,resultado.indexOf('-')));
-        let n2 = Number(resultado.substring(resultado.indexOf('-')+1, resultado.length));
-        subtrair(n1,n2)
-    }else if(resultado.indexOf('/') != -1){
-        let n1 = Number(resultado.substring(0,resultado.indexOf('/')));
-        let n2 = Number(resultado.substring(resultado.indexOf('/')+1, resultado.length));
-        dividir(n1,n2)
-    }else if(resultado.indexOf('*') != -1){
-        let n1 = Number(resultado.substring(0,resultado.indexOf('*')));
-        let n2 = Number(resultado.substring(resultado.indexOf('*')+1, resultado.length));
-        multiplicar(n1,n2)
-    }else{
+    } else {
         clean();
     }
 }
 
-function somar(n1, n2){
+function somar(n1, n2) {
     let resultado = n1 + n2;
     resultado = resultado.toString().replaceAll('.', ',');
     document.getElementById("display").innerHTML = resultado;
 }
 
-function subtrair(n1, n2){
-    
-    let resultado = n1 + n2;
+function subtrair(n1, n2) {
+    let resultado = n1 - n2;
     resultado = resultado.toString().replaceAll('.', ',');
     document.getElementById("display").innerHTML = resultado;
 }
 
-function dividir(n1, n2){
-    
+function dividir(n1, n2) {
     let resultado = n1 / n2;
     resultado = resultado.toString().replaceAll('.', ',');
     document.getElementById("display").innerHTML = resultado;
 }
 
-function multiplicar(n1, n2){
+function multiplicar(n1, n2) {
     let resultado = n1 * n2;
     resultado = resultado.toString().replaceAll('.', ',');
     document.getElementById("display").innerHTML = resultado;
 }
 
-function inverteSinal(){
+function inverteSinal() {
     let resultado = document.getElementById("display").innerHTML;
 
-    if(resultado[0] == '-'){
-        resultado = resultado.substring(1,display.length);
-    }else{
-        resultado[0] = '-';
+    if (resultado[0] == '-') {
+        resultado = resultado.substring(1, display.length);
+        document.getElementById("display").innerHTML = resultado;
+    } else {
+        resultado = '-' + resultado;
+        document.getElementById("display").innerHTML = resultado;
     }
+}
+
+function verificaSinais(texto) {
+    if (texto[0] == '*' || texto[0] == '/') {
+        return false;
+    } else if (texto.indexOf("+") != -1) {
+        let pos = texto.indexOf("+");
+
+        if (texto[pos - 1] == '+' || texto[pos - 1] == '-' || texto[pos - 1] == '/' || texto[pos - 1] == '*') {
+            return false;
+        } else if (texto[pos + 1] == '+' || texto[pos + 1] == '-' || texto[pos + 1] == '/' || texto[pos + 1] == '*') {
+            return false;
+        }
+    }
+
+    if (texto.indexOf("-") != -1) {
+        let pos = texto.indexOf("-");
+
+        if (texto[pos - 1] == '+' || texto[pos - 1] == '-' || texto[pos - 1] == '/' || texto[pos - 1] == '*') {
+            return false;
+        } else if (texto[pos + 1] == '+' || texto[pos + 1] == '-' || texto[pos + 1] == '/' || texto[pos + 1] == '*') {
+            return false;
+        }
+    }
+
+    if (texto.indexOf("/") != -1) {
+        let pos = texto.indexOf("/");
+
+        if (texto[pos - 1] == '+' || texto[pos - 1] == '-' || texto[pos - 1] == '/' || texto[pos - 1] == '*') {
+            return false;
+        } else if (texto[pos + 1] == '+' || texto[pos + 1] == '-' || texto[pos + 1] == '/' || texto[pos + 1] == '*') {
+            return false;
+        }
+    }
+
+    if (texto.indexOf("*") != -1) {
+        let pos = texto.indexOf("*");
+
+        if (texto[pos - 1] == '+' || texto[pos - 1] == '-' || texto[pos - 1] == '/' || texto[pos - 1] == '*') {
+            return false;
+        } else if (texto[pos + 1] == '+' || texto[pos + 1] == '-' || texto[pos + 1] == '/' || texto[pos + 1] == '*') {
+            return false;
+        }
+    }
+
+    return true;
 }
