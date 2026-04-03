48f039ec9da57f5db93e99f2438547844f832ed4
Jerso06 <jedsonhenrique2006@gmail.com>
2026-04-03 15:18:31 -0300

    finalização do exercício da calculadora com js

--- Desenvolvimento Web 1/abril/01/Calculadora/script.js
+++ Desenvolvimento Web 1/abril/01/Calculadora/script.js
@@ -3,13 +3,35 @@ function clean() {
 }
 
 function inserir(num) {
+    const operadores = ['+', '-', '*', '/'];
+    let resultado = document.getElementById("display").innerHTML;
+    if (operadores.includes(num)) {
+        if (resultado.length == 0) {
+            if (num == '-') {
+                document.getElementById("display").innerHTML += num;
+            }
+            return;
+        } else if (num == '-' && resultado[resultado.length - 1] == '/' || num == '-' && resultado[resultado.length - 1] == '*') {
+            document.getElementById("display").innerHTML += num;
+            return;
+        } else if (resultado[resultado.length - 1] == '-' && operadores.includes(resultado[resultado.length - 2])) {
+            if (num != '-') {
+                resultado = resultado.substring(0, resultado.length - 1);
+                document.getElementById("display").innerHTML = resultado;
+            }
+            return;
+        } else if (operadores.includes(resultado[resultado.length - 1])) {
+            document.getElementById("display").innerHTML = resultado.substring(0, resultado.length - 1) + num;
+            return;
+        }
+    }
     document.getElementById("display").innerHTML += num;
 }
 
 function back() {
-    let display = document.getElementById("display").innerHTML;
+    let resultado = document.getElementById("display").innerHTML;
 
-    document.getElementById("display").innerHTML = display.substring(0, display.length - 1);
+    document.getElementById("display").innerHTML = resultado.substring(0, resultado.length - 1);
 }
 
 function calcular() {
@@ -17,65 +39,96 @@ function calcular() {
     resultado = resultado.replaceAll(',', '.');
     let n1, n2;
 
-    if (resultado.length != 0 && verificaSinais(resultado)) {
-        if (resultado.indexOf('+') != -1) {
-            n1 = Number(resultado.substring(0, resultado.indexOf('+')));
-            n2 = Number(resultado.substring(resultado.indexOf('+') + 1, resultado.length));
-            somar(n1, n2);
-        } else if (resultado.includes('-')) {
-            let indiceOperador = resultado.indexOf('-', 1);
-
-            if (indiceOperador != -1) {
-                n1 = Number(resultado.substring(0, indiceOperador));
-                n2 = Number(resultado.substring(indiceOperador + 1, resultado.length));
-                subtrair(n1, n2);
-            } else if (resultado.startsWith('-')) {
+    if (resultado.length != 0) {
+        if (verificaSinais(resultado)) {
+            if (resultado.indexOf('+', 1) != -1) {
+                if (!isNaN(n1 = Number(resultado.substring(0, resultado.indexOf('+', 1)))) &&
+                    !isNaN(n2 = Number(resultado.substring(resultado.indexOf('+', 1) + 1, resultado.length)))) {
+                    somar(n1, n2);
+                } else {
+                    resultado = "Erro no Cálculo";
+                    document.getElementById("display").innerHTML = resultado;
+                    setTimeout(function () {
+                        clean();
+                    }, 2000);
+                }
+            } else if (resultado.indexOf('/') != -1) {
+                if (!isNaN(n1 = Number(resultado.substring(0, resultado.indexOf('/', 1)))) &&
+                    !isNaN(n2 = Number(resultado.substring(resultado.indexOf('/', 1) + 1, resultado.length)))) {
+                    dividir(n1, n2);
+                } else {
+                    resultado = "Erro no Cálculo";
+                    document.getElementById("display").innerHTML = resultado;
+                    setTimeout(function () {
+                        clean();
+                    }, 2000);
+                }
+            } else if (resultado.indexOf('*') != -1) {
+                if (!isNaN(n1 = Number(resultado.substring(0, resultado.indexOf('*', 1)))) &&
+                    !isNaN(n2 = Number(resultado.substring(resultado.indexOf('*', 1) + 1, resultado.length)))) {
+                    multiplicar(n1, n2);
+                } else {
+                    resultado = "Erro no Cálculo";
+                    document.getElementById("display").innerHTML = resultado;
+                    setTimeout(function () {
+                        clean();
+                    }, 2000);
+                }
+            } else if (resultado.indexOf('-', 1) != -1) {
+                if (!isNaN(n1 = Number(resultado.substring(0, resultado.indexOf('-', 1)))) &&
+                    !isNaN(n2 = Number(resultado.substring(resultado.indexOf('-', 1) + 1, resultado.length)))) {
+                    subtrair(n1, n2);
+                } else {
+                    resultado = "Erro no Cálculo";
+                    document.getElementById("display").innerHTML = resultado;
+                    setTimeout(function () {
+                        clean();
+                    }, 2000);
+                }
+            } else {
                 finalizarExibicao(resultado);
             }
-        } else if (resultado.indexOf('/') != -1) {
-            n1 = Number(resultado.substring(0, resultado.indexOf('/')));
-            n2 = Number(resultado.substring(resultado.indexOf('/') + 1, resultado.length));
-            dividir(n1, n2);
-        } else if (resultado.indexOf('*') != -1) {
-            n1 = Number(resultado.substring(0, resultado.indexOf('*')));
-            n2 = Number(resultado.substring(resultado.indexOf('*') + 1, resultado.length));
-            multiplicar(n1, n2);
-        } 
-        else {
-            finalizarExibicao(resultado);
+        } else {
+            resultado = "Erro de Formatação";
+            document.getElementById("display").innerHTML = resultado;
+            setTimeout(function () {
+                clean();
+            }, 2000);
         }
-    } else {
-        clean();
     }
 }
 
 function finalizarExibicao(valor) {
+    if (isNaN(valor)) {
+        document.getElementById("display").innerHTML = "Múltiplas Operações";
+        setTimeout(function () {
+            clean();
+        }, 2000);
+        return;
+    }
+
     valor = valor.toString().replaceAll('.', ',');
     document.getElementById("display").innerHTML = valor;
 }
 
 function somar(n1, n2) {
     let resultado = n1 + n2;
-    resultado = resultado.toString().replaceAll('.', ',');
-    document.getElementById("display").innerHTML = resultado;
+    finalizarExibicao(resultado);
 }
 
 function subtrair(n1, n2) {
     let resultado = n1 - n2;
-    resultado = resultado.toString().replaceAll('.', ',');
-    document.getElementById("display").innerHTML = resultado;
+    finalizarExibicao(resultado);
 }
 
 function dividir(n1, n2) {
     let resultado = n1 / n2;
-    resultado = resultado.toString().replaceAll('.', ',');
-    document.getElementById("display").innerHTML = resultado;
+    finalizarExibicao(resultado);
 }
 
 function multiplicar(n1, n2) {
     let resultado = n1 * n2;
-    resultado = resultado.toString().replaceAll('.', ',');
-    document.getElementById("display").innerHTML = resultado;
+    finalizarExibicao(resultado);
 }
 
 function inverteSinal() {
@@ -89,30 +142,12 @@ function inverteSinal() {
 }
 
 function verificaSinais(texto) {
-    if (texto[0] == '*' || texto[0] == '/') {
-        return false;
-    }
-
     const operadores = ['+', '-', '*', '/'];
 
-    for (let i = 0; i < texto.length; i++) {
-        let caractereAtual = texto[i];
-        let proximoCaractere = texto[i + 1];
-
-        if (operadores.includes(caractereAtual)) {
-            
-            if (operadores.includes(proximoCaractere) && proximoCaractere !== '-') {
-                return false;
-            }
-
-            if (proximoCaractere === '-' && operadores.includes(texto[i + 2])) {
-                return false;
-            }
-
-            if (i === texto.length - 1) {
-                return false;
-            }
-        }
+    if (texto[0] == '*' || texto[0] == '/' || texto[0] == '.') {
+        return false;
+    } else if (operadores.includes(texto[texto.length - 1])) {
+        return false;
     }
 
     return true;
