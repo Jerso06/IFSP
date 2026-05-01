b8638ee35d79472beda4c0bf457c2412d18c4966
Jerso06 <jedsonhenrique2006@gmail.com>
2026-05-01 01:07:08 -0300

    finalização da revisão de web

--- Desenvolvimento Web 1/03 - abril/29/revisao/script.js
+++ Desenvolvimento Web 1/03 - abril/29/revisao/script.js
@@ -1,23 +1,25 @@
-function passarCampo(idCampo){
+function passarCampo(idCampo) {
     let campo = document.getElementById(`${idCampo}`);
     let listaD = document.getElementById("listaDisponiveis");
     let listaR = document.getElementById("listaRelatorios");
 
-    
-    if(listaD.contains(campo)){
+
+    if (listaD.contains(campo)) {
+        campo.classList.remove("campoD");
+        campo.classList.add("campoR");
         listaR.appendChild(campo);
         listaD.removeChild(campo);
-    }else{
+    } else {
+        campo.classList.add("campoD");
+        campo.classList.remove("campoR");
         listaD.appendChild(campo);
         listaR.removeChild(campo);
     }
 }
 
-async function gerarTabela(){
+async function gerarTabela() {
     let tabela = document.getElementById("tabelaResultado");
     tabela.innerHTML = "";
-    let thead = document.createElement("thead");
-    let linhaHead = document.createElement('tr');
     let verificaID = 0;
     let verificaNome = 0;
     let verificaCidade = 0;
@@ -26,28 +28,42 @@ async function gerarTabela(){
     let campos = document.querySelectorAll("#listaRelatorios li");
     campos.forEach(item => {
         if (item.id === "idRegistro") {
-            let item = document.createElement('th');
-            item.innerHTML = "ID";
-            linhaHead.appendChild(item);
             verificaID = 1;
-        }else if(item.id === "nome"){
-            let item = document.createElement('th');
-            item.innerHTML = "Nome";
-            linhaHead.appendChild(item);
+        } else if (item.id === "nome") {
             verificaNome = 1;
-        }else if(item.id === "cidade"){
-            let item = document.createElement('th');
-            item.innerHTML = "Cidade";
-            linhaHead.appendChild(item);
+        } else if (item.id === "cidade") {
             verificaCidade = 1;
-        }else if(item.id === "valor"){
-            let item = document.createElement('th');
-            item.innerHTML = "valor";
-            linhaHead.appendChild(item);
+        } else if (item.id === "valor") {
             verificaValor = 1;
         }
     });
+
+
+    let thead = document.createElement("thead");
+    let linhaHead = document.createElement('tr');
+
+    if (verificaID) {
+        let item = document.createElement('th');
+        item.innerHTML = "ID";
+        linhaHead.appendChild(item);
+    }
+    if (verificaNome) {
+        let item = document.createElement('th');
+        item.innerHTML = "Nome";
+        linhaHead.appendChild(item);
+    }
+    if (verificaCidade) {
+        let item = document.createElement('th');
+        item.innerHTML = "Cidade";
+        linhaHead.appendChild(item);
+    }
+    if (verificaValor) {
+        let item = document.createElement('th');
+        item.innerHTML = "Valor";
+        linhaHead.appendChild(item);
+    }
     thead.appendChild(linhaHead);
+    tabela.appendChild(thead);
 
     let dados = await fetch('dados.json');
     dados = await dados.json();
@@ -56,25 +72,25 @@ async function gerarTabela(){
     dados.forEach(objeto => {
         let linha = document.createElement('tr');
 
-        if(verificaID){
+        if (verificaID) {
             let id = document.createElement('td');
             id.innerHTML = objeto.id;
             linha.appendChild(id);
         }
 
-        if(verificaNome){
+        if (verificaNome) {
             let nome = document.createElement('td');
             nome.innerHTML = objeto.nome;
             linha.appendChild(nome);
         }
 
-        if(verificaCidade){
+        if (verificaCidade) {
             let cidade = document.createElement('td');
             cidade.innerHTML = objeto.cidade;
             linha.appendChild(cidade);
         }
 
-        if(verificaValor){
+        if (verificaValor) {
             let valor = document.createElement('td');
             valor.innerHTML = objeto.venda;
             linha.appendChild(valor);
