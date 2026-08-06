e53f9a6e0d1f5eb0ad6498822f4ba223fb083df9
Jerso06 <jedsonhenrique2006@gmail.com>
2026-08-06 20:25:01 -0300

    exemplo de calculadora node

---
+++ Desenvolvimento Web 2/08 - agosto/06/calc.js
@@ -0,0 +1,16 @@
+function soma (a, b) {
+return a + b;
+}
+function subtracao (a, b) {
+return a -b;
+}
+function multiplicacao (a, b) {
+return a * b;
+}
+function divisao(a, b) {
+return a / b;
+}
+console.log(soma (1, 2)); // 3
+console.log(subtracao (1, 2)); // -1
+console.log(multiplicacao (1, 2)); // 2
+console.log(divisao (1, 2)); // 0.5
